40f6179647b8f510c3aeefe97d8761f4eed7d6c4
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2025-02-22 12:58:49 +0300

    req

---
+++ dere_react_app/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+ return(
+  <header>
+   <h1> my react website</h1>
+   <nav>
+    <ul>
+     <li><a herf="#">Home</a></li>
+     <li><a herf="#">About</a></li>
+     <li><a herf="#"> Services</a></li>
+     <li><a herf="#">Contact</a></li>
+    </ul>
+   </nav>
+  </header>
+ );
+
+}
+
+export default Header
